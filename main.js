fbd6e3203a0588fdd77de5034a83e429e0f7e9a6
Amol Venkataraman <amolven16@gmail.com>
2022-04-09 20:31:07 -0400

    Added save and load functions

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const { app, BrowserWindow } = require("electron");
-const { createReadStream } = require("original-fs");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const fs = require("original-fs");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      nodeIntegrationInSubFrames: true,
+      enableRemoteModule: true,
+      contextIsolation: false //required flag
+    }
   });
   //win.setMenu(null);
   win.maximize();
@@ -28,4 +35,74 @@ app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
+});
+
+ipcMain.on("save-file", (event, arg) => {
+  const options = {
+    title: "Save File",
+    defaultPath: "./",
+    buttonLabel: "Save",
+    filters: [
+      {
+        name: "JSON",
+        extensions: ["json"]
+      }
+    ]
+  };
+
+  dialog.showSaveDialog({
+    title: "Save File",
+    defaultPath: "./",
+    buttonLabel: "Save",
+    filters: [
+      {
+        name: "JSON",
+        extensions: ["json"]
+      }
+    ]
+  }).then(file => {
+    if (file) {
+      fs.writeFile(file.filePath.toString(), JSON.stringify(arg), err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  });
+});
+
+ipcMain.on("load-file", (event, arg) => {
+  const options = {
+    title: "Load File",
+    defaultPath: "./",
+    buttonLabel: "Load",
+    filters: [
+      {
+        name: "JSON",
+        extensions: ["json"]
+      }
+    ]
+  };
+
+  dialog.showOpenDialog({
+    title: "Load File",
+    defaultPath: "./",
+    buttonLabel: "Load",
+    filters: [
+      {
+        name: "JSON",
+        extensions: ["json"]
+      }
+    ]
+  }).then(file => {
+    if (file) {
+      fs.readFile(file.filePaths[0], (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          event.sender.send("load-file", JSON.parse(data));
+        }
+      });
+    }
+  });
 });
